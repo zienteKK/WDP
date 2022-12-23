990dca9100b50509440e57fecb4679087c5e5f81
zienteKK <zienowiczpiotr@gmail.com>
2022-12-23 10:20:22 +0100

    all

---
+++ wdp11/Rekurencja/Rekurencja.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+
+/*int sumaLiczb(int n) {
+    if (n < 1) {
+        cout << "\nThe sum of numbers is: ";
+        return 0;
+    }
+    cout << n << " ";
+    return n + sumaLiczb(n - 1);
+}*/
+/*int factorial(int n) {
+    if (n == 0) {
+        
+        return 1;
+    }
+    else if (n < 0) {
+        cout << " You've entered negative number!";
+        return 0;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}*/
+/*int elementCiag(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    else {
+        return (-n * elementCiag(n - 1) + 4);
+    }
+}*/
+int Fibb(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    else if (n == 1) {
+        return 1;
+    }
+    else if (n < 0) {
+        return 0;
+    }
+    else {
+        return Fibb(n - 1) + Fibb(n - 2);
+    }
+}
+int main()
+{
+    int n, result;
+    cout << "Enter a number: ";
+        cin >> n;
+
+       // result = sumaLiczb(n);
+        //result = factorial(n);
+        //result = elementCiag(n);
+        result = Fibb(n);
+        cout << "Wynik dla n = " << n <<" wynosi: ";
+        cout << result;
+}
+
---
+++ x-mas tree/x-mas tree/Lekcja11.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int height;
+
+    cout << "The height of the tree: ";
+    cin >> height;
+    cin.ignore();
+
+    cout << endl << flush;
+
+    int colour[5] = { 31, 34, 35, 36, 37 };
+    for (int r = height; r >= 0; r--) {
+        for (int c = 0; c < r; c++) {
+            cout << " ";
+        }
+        for (int c = r * 2; c < height * 2 - 1; c++) {
+            if (((int)(rand() % 1000 + 1)) > 950) {
+                int i = (int)(rand() % 5 + 1);
+                cout << "\033[1;" << colour[i] << "m●\033[0m";
+            }
+            else {
+                cout << "\033[0;32m.\033[0m";
+            }
+        }
+        cout << endl;
+    }
+
+    int base = height * 2 - 1;
+    int logg = (int)base / 5;
+    if (logg == 0)
+        logg = 1;
+    if (logg % 2 == 0)
+        logg += 1;
+
+    int log_init = (int)(height - (logg / 2)) - 1;
+    for (int r = 0; r < logg; r++) {
+        for (int c = 0; c < log_init; c++) {
+            cout << " ";
+        }
+        for (int c = 0; c < logg; c++) {
+            cout << "\033[0;33m#\033[0m";
+        }
+        cout << endl;
+    }
+
+    cin.ignore();
+
+    return 0;
+}
